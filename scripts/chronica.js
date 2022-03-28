b7ff25c08ba2ac0bc0513d2eaa6faad2c505beb6
Tobias Franz <cinex.mail@web.de>
2022-03-28 16:35:09 +0200

    Added a simple Button to be expanded upon.
    Signed-off-by: Tobias Franz <tobias.franz@mydata.de>

--- scripts/chronica.js
+++ scripts/chronica.js
@@ -1,3 +1,5 @@
-Hooks.once('ready', async function () {
-    console.log("The module works!")
+Hooks.on("renderSidebarTab", async (app, html) => {
+    if (app.options.id === "journal" && game.user.isGM) {
+        html.find("footer").append('<div class="footer-actions action-buttons flexrow"><button class="create-folder"><i class="fas fa-file-import"></i> Import Chronica Data</button></div>')
+    }
 })
